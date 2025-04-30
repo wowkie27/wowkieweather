b7d2371a7d2861ad947b14906410c853f5693aa9
wowkie <mr.arseniy.kazakov@mail.ru>
2025-05-01 00:44:00 +0300

    version 1.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wowkieweather/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
